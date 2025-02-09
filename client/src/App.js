20401668997fe5a5725fbf5fc9f02eb8562c276e
yyujinparkk <yujin23841@gmail.com>
2025-02-08 23:29:17 -0500

    search bar clickability

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  // Import BrowserRouter
 import { ContentPage } from './ContentPage.js';
 import { HomePage } from './HomePage.js';
 
+
 function App() {
-  return (
-    <div className="App">
-      <ContentPage />
-    </div>
-  );
+ return (
+   <div className="App">
+     <BrowserRouter>  {/* Wrap your routes in BrowserRouter */}
+       <Routes>
+         <Route path='/' element={<HomePage />} />
+         <Route path='/content' element={<ContentPage />} />
+       </Routes>
+     </BrowserRouter>
+   </div>
+ );
 }
+
 
 export default App;
